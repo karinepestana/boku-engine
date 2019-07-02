bf95cfca2be7f8c7fa8037b6584ee7c74c3aee33
karinepestana <karine_pestana_34@hotmail.com>
2019-07-02 12:22:56 -0300

    poda alpha beta

--- my_client.py
+++ my_client.py
@@ -228,7 +228,7 @@ def heuristic(board, player):
     return -5           
 
 
-def minimax(board, depth, player, depth_initial): 
+def minimax(board, depth, player, depth_initial, alpha=-inf, beta=inf, inv_move = (-1,-1)):
     """ 
     Minimax algorithm that choose the best movement in board
     Args:
@@ -250,13 +250,10 @@ def minimax(board, depth, player, depth_initial):
             return 10 + depth, board
     if depth == depth_initial-2:
         return h, board 
-    #print(player)
-    #print("BOARD")
-    #print(board)
-    #print("MOVE")
+
 
     if player == "2":
-        best_val = -inf
+        best_val = inf
         best_mov = None
         for move in get_available_moves(board, player):
             #print(move)
@@ -265,31 +262,158 @@ def minimax(board, depth, player, depth_initial):
             column, line = move
             board_cpy[column-1][line-1] = 2
             #print(board_cpy)
-            value, mov = minimax(board_cpy, depth-1, "1", depth_initial)
-            if best_val < value:
-                best_val = value
+            value, mov = minimax(board_cpy, depth-1, "1", depth_initial, alpha, beta)
+            
+            if best_val > value:
                 best_mov = move
+
+            best_val = min(value, best_val)
+            beta = min(beta, best_val)
+
+            if alpha >= beta:
+                break
+            
         return best_val, best_mov
     
     else:
-        best_val = inf
+        best_val = -inf
         best_mov = None
         for move in get_available_moves(board, player):
-            #print(move)
-            #print(get_available_moves(board))
             board_cpy = copy.deepcopy(board)
             column, line = move
             board_cpy[column-1][line-1] = 1
             #print(board_cpy)
-            value, mov = minimax(board_cpy, depth-1, "2", depth_initial)
-            if best_val > value:
-                best_val = value
+            value, mov = minimax(board_cpy, depth-1, "2", depth_initial, alpha, beta)
+            if best_val < value:
                 best_mov = move
+
+            best_val = max(value, best_val)
+            alpha = max(alpha, best_val)
+
+            if alpha >= beta:
+                break
         return best_val, best_mov
 
+def can_remove(board, player):
+        removals = []
+        l = []
 
+        #test vertical
+        
+        #test upward
+        s = ""
+        for line in range(max(last_line-3,1), last_line+1):
+          
+            state = board[last_column-1][line-1]
+            s += str(state)
+        
+        if ("1221" in s and player==1) or ("2112" in s and player==2):
+            removals.append([(last_column,last_line-1),(last_column,last_line-2)])
 
+        #test downward
+        s = ""
+        for line in range(last_line,  min(last_line+3,len(board[last_column-1]))+1):
+        
+            state = board[last_column-1][line-1]
+            s += str(state)
+        
+        if ("1221" in s and player==1) or ("2112" in s and player==2):
+            removals.append([(last_column,last_line+1),(last_column,last_line+2)])
+                 
 
+        # test upward diagonals
+        diags = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
+                 (2, 6), (3, 7), (4, 8), (5, 9), (6, 10)]
+
+        col = last_column
+        line =last_line
+        coords = (col, line)
+
+        s = ""
+        for i in range(0, 4):
+            column = coords[0]
+            line = coords[1]
+            state = board[column - 1][line - 1]
+            l.append((column, line))
+            s += str(state)
+            if "1221" in s and player == 1:
+                removals.append(l[-3:-1])
+            if "2112" in s and player == 2:
+                removals.append(l[-3:-1])
+            coords = neighbors(board, column, line)[1]
+            if coords == None:
+                break
+
+        col = last_column
+        line = last_line
+        coords = (col, line)
+
+        s = ""
+        for i in range(0, 4):
+            print(coords)
+            column = coords[0]
+            line = coords[1]
+            state = board[column - 1][line - 1]
+            l.append((column, line))
+            s += str(state)
+            print(s)
+            if "1221" in s and player == 1:
+                removals.append(l[-3:-1])
+            if "2112" in s and player == 2:
+                removals.append(l[-3:-1])
+            coords = neighbors(board, column, line)[5]
+            if coords == None:
+                break
+
+        # test downward diagonals
+        diags = [(6, 1), (5, 1), (4, 1), (3, 1), (2, 1),
+                 (1, 1), (1, 2), (1, 3), (1, 4), (1, 5)]
+
+        col = last_column
+        line = last_line
+        coords = (col, line)
+
+        s = ""
+        for i in range(0, 4):
+            column = coords[0]
+            line = coords[1]
+            state = board[column - 1][line - 1]
+            l.append((column, line))
+            s += str(state)
+            if "1221" in s and player == 1:
+                removals.append(l[-3:-1])
+            if "2112" in s and player == 2:
+                removals.append(l[-3:-1])
+            coords = neighbors(board, column, line)[2]
+            if coords == None:
+                break
+
+        col = last_column
+        line = last_line
+        coords = (col, line)
+
+        s = ""
+        for i in range(0, 4):
+            print(coords)
+            column = coords[0]
+            line = coords[1]
+            state = board[column - 1][line - 1]
+            l.append((column, line))
+            s += str(state)
+            print(s)
+            if "1221" in s and player == 1:
+                removals.append(l[-3:-1])
+            if "2112" in s and player == 2:
+                removals.append(l[-3:-1])
+            coords = neighbors(board, column, line)[4]
+            if coords == None:
+                break
+
+        if len(removals) > 0:
+            removals = [item for sublist in removals for item in sublist]
+            return removals
+        else:
+            return None
 
 if len(sys.argv)==1:
     print("Voce deve especificar o numero do jogador (1 ou 2)\n\nExemplo:    ./random_client.py 1")
@@ -336,6 +460,16 @@ while not done:
         movimento = minimax(board, len(movimentos), str(player), len(movimentos))
         resp = urllib.request.urlopen("%s/move?player=%d&coluna=%d&linha=%d" % (host,player,movimento[1][0],movimento[1][1]))
         msg = eval(resp.read())
+        #print("mensagem" + strmsg)
+
+        if msg[0] == 2:
+            print("sanduiche")
+            resp = urllib.request.urlopen("%s/tabuleiro" % host)
+            board = eval(resp.read())
+            movimento = can_remove(board, player)
+            print(movimento)
+            resp = urllib.request.urlopen("%s/move?player=%d&coluna=%d&linha=%d" % (host,player,movimento[1][0],movimento[1][1]))
+            msg = eval(resp.read())
 
         # Se com o movimento o jogo acabou, o cliente venceu
         if msg[0]==0:
